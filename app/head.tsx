bacf5450c099705bff550c3d9eb3e1d5c9bc4eb1
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2022-12-15 10:29:20 +0100

    description + robots meta tags (lighthouse score)

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,11 @@ export default function Head() {
     <>
       <title>The Blog</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        content="description"
+        name="Un blog réalisé en Next.js 13 + Typescript + Tailwind + Sanity Studio v3 headless CMS"
+      />
+      <meta name="robots" content="all" />
       <link rel="icon" href="/favicon.ico" />
     </>
   )
