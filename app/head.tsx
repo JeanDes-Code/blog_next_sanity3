f276bf2092c442b92419fbec0585fbf1ebe14495
JeanDes-Code <78590821+JeanDes-Code@users.noreply.github.com>
2022-12-15 10:32:06 +0100

    oups name="description" content="seocontent"

--- app/head.tsx
+++ app/head.tsx
@@ -4,8 +4,8 @@ export default function Head() {
       <title>The Blog</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta
-        content="description"
-        name="Un blog réalisé en Next.js 13 + Typescript + Tailwind + Sanity Studio v3 headless CMS"
+        name="description"
+        content="Un blog réalisé en Next.js 13 + Typescript + Tailwind + Sanity Studio v3 headless CMS"
       />
       <meta name="robots" content="all" />
       <link rel="icon" href="/favicon.ico" />
